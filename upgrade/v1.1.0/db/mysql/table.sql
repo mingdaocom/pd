992e5fbea4a66a37a0ced526bf8a001fcebe003d
金彪 <1798410296@qq.com>
2020-04-22 12:01:05 +0800

    Add 如何升级到 v1.1.0.md

---
+++ upgrade/v1.1.0/db/mysql/table.sql
@@ -0,0 +1,37 @@
+﻿USE MDProject;
+
+-- 添加 通讯录规则表
+-- ----------------------------
+-- Table structure for Project_UserRule
+-- ----------------------------
+CREATE TABLE `Project_UserRule` (
+  `AutoId` int(11) NOT NULL AUTO_INCREMENT,
+  `ProjectId` char(36) NOT NULL,
+  `RuleId` char(36) NOT NULL,
+  `CreateTime` datetime(6) NOT NULL,
+  `CreateUser` char(36) NOT NULL,
+  `UpdateTime` datetime(6) NOT NULL,
+  `LastModifyUser` char(36) NOT NULL,
+  PRIMARY KEY (`AutoId`),
+  KEY `IX_ProjectId` (`ProjectId`) USING BTREE,
+  KEY `IX_RuleId` (`RuleId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- 添加 通讯录规则项表
+-- ----------------------------
+-- Table structure for Project_UserRuleItem
+-- ----------------------------
+CREATE TABLE `Project_UserRuleItem` (
+  `AutoId` int(11) NOT NULL AUTO_INCREMENT,
+  `ProjectId` char(36) NOT NULL,
+  `RuleId` char(36) NOT NULL,
+  `RuleItemType` int(11) NOT NULL DEFAULT '0',
+  `TargetId` varchar(50) NOT NULL,
+  `TargetType` int(11) NOT NULL DEFAULT '0',
+  `CreateTime` datetime(6) NOT NULL,
+  `CreateUser` char(36) NOT NULL,
+  PRIMARY KEY (`AutoId`),
+  KEY `IX_ProjectId` (`ProjectId`) USING BTREE,
+  KEY `IX_Rule_Project` (`ProjectId`,`RuleId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
