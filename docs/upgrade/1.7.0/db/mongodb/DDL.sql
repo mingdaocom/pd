3c194f868a8272975d532228aa98c82773a72f9f
beckjin <1798410296@qq.com>
2020-09-09 12:48:13 +0800

    Update DDL.sql

--- docs/upgrade/1.7.0/db/mongodb/DDL.sql
+++ docs/upgrade/1.7.0/db/mongodb/DDL.sql
@@ -1,4 +1,3 @@
 use mdworksheet;
 
-db.customicon.createIndex({ "pid": 1}, { background: true, name: "ix_pid" });
 db.customicon.createIndex({ "pid": 1, "name": 1,}, { background: true, name: "ix_pid_name" });
